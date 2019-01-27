37b448ee024414ffcf4e178aa19aa149fd223451
GlassToeStudio <GlassToeStudio@gmail.com>
2019-01-26 22:31:14 -0600

    Add usage checks and static fields to attribute

--- Attributes/MoveablePointAttribute.cs
+++ Attributes/MoveablePointAttribute.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-//Allows for the use of [MoveablePoint] on all Vector2 and Vector3 type variables. Including []'s and List<>
+/// <summary>
+/// Allows for the use of [MoveablePoint] on all Vector2 and Vector3 type variables. Including []'s and List<>'s
+/// </summary>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Struct)]
 public class MoveablePointAttribute : PropertyAttribute
 {
-
+    public static bool ShowHandles = true;
 }
