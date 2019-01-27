a6f2f019e04b5078d771f0552dddb856a3317f14
GlassToeStudio <GlassToeStudio@gmail.com>
2019-01-26 22:37:00 -0600

    Add moveable point menus

---
+++ Editor/MoveablePointMenus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+public class MoveablePointMenus : Editor
+{
+    [MenuItem("Custom/MoveablePoint/ShowHandles")]
+    private static void ShowHandles()
+    {
+        MoveablePointAttribute.ShowHandles = !MoveablePointAttribute.ShowHandles;
+        SceneView.lastActiveSceneView.Repaint();
+    }
+    [MenuItem("Custom/MoveablePoint/ShowHandles", validate = true)]
+    private static bool ShowHandlesValidator()
+    {
+        return !MoveablePointAttribute.ShowHandles;
+    }
+
+    [MenuItem("Custom/MoveablePoint/HideHandles")]
+    private static void HideHandles()
+    {
+        MoveablePointAttribute.ShowHandles = !MoveablePointAttribute.ShowHandles;
+        SceneView.lastActiveSceneView.Repaint();
+    }
+    [MenuItem("Custom/MoveablePoint/HideHandles", validate = true)]
+    private static bool HideHandlesValidator()
+    {
+        return MoveablePointAttribute.ShowHandles;
+    }
+}
